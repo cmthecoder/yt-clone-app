ec2b4a5619557d7799139b0852b83552c80102be
Chris Mems <christopher.mems@.com>
2023-03-15 11:30:12 -0500

    display channel component and style it

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,6 +1,4 @@
 import { Stack, Box } from "@mui/material"
-// import ChannelCard from "./ChannelCard"
-// import VideoCard from "./VideoCard"
 import { VideoCard, ChannelCard } from './'
 
 const Videos = ({ videos }) => {
@@ -10,7 +8,7 @@ const Videos = ({ videos }) => {
       {videos.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} />}
-          {/* {item.id.channelId && <ChannelCard channelDetail={item} />} */}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
         </Box>
       ))}
     </Stack>

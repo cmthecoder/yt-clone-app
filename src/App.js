dc4ea5a882a733d02dedc19b740986d8b8d0c83d
Chris Mems <christopher.mems@.com>
2023-03-15 07:06:58 -0500

    create components and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { NavBar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components';
 
-import React from 'react'
-
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
+  <Box sx={{ backgroundColor: '#000' }}>
+    <NavBar />
+    <Routes>
+      <Route
+        path='/'
+        exact element={<Feed />}
+      />
+      <Route
+        path='/video/:id'
+        exact element={<VideoDetail />}
+      />
+      <Route
+        path='/channel/:id'
+        exact element={<ChannelDetail />}
+      />
+      <Route
+        path='/search/:searchTerm'
+        exact element={<SearchFeed />}
+      />
+    </Routes>
+  </Box>
+)
 
 export default App
